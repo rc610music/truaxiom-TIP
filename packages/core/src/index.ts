6bd2fa0ed9d0deee5a4a8ffd8f3e3b08e1be0dac
rc610music <75897961+rc610music@users.noreply.github.com>
2026-08-10 22:29:36 -0400

    Export content map and ingestion runner modules

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,3 +4,5 @@ export * from "./organizationalBrain";
 export * from "./taskQueue";
 export * from "./recommendations";
 export * from "./rootWorkIngestion";
+export * from "./rootWorkContentMap";
+export * from "./ingestionRunner";
