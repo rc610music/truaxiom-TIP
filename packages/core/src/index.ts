a72b748b72a0e9c0f7d688c8c5b5b9d2e4623e50
rc610music <75897961+rc610music@users.noreply.github.com>
2026-08-10 22:24:38 -0400

    Export Sprint 002 task recommendation and ingestion helpers

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,6 @@
 export * from "./seed";
 export * from "./knowledgeGraph";
 export * from "./organizationalBrain";
+export * from "./taskQueue";
+export * from "./recommendations";
+export * from "./rootWorkIngestion";
